3af9c404033513c1a5a7459f7a9b31726bbad9cf
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-05-24 15:35:51 +0530

    created a textDecor class

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 // decorations for widgets
+
+// Text Decorations
+
+// Title Text Decoration
+
+class TextDecor {
+  static const TextStyle titleTextDecoration = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+}
