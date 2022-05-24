1617503715711c695e17f9f22cfab2309ab2770d
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-05-24 15:34:22 +0530

    created form heading widget

---
+++ lib/screens/widgets/form_heading.dart
@@ -0,0 +1,47 @@
+import 'package:classify/assets/asset_register.dart';
+import 'package:classify/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class FormHeading extends StatelessWidget {
+  final String name;
+  const FormHeading({Key? key, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var ss = MediaQuery.of(context).size;
+    return Center(
+      child: SizedBox(
+        width: ss.width * 0.8,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: ss.width * 0.20,
+              child: Image.asset(
+                AssetRegister.logoImg,
+                filterQuality: FilterQuality.high,
+                isAntiAlias: true,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                  left: 10, right: 20, top: 10, bottom: 10),
+              color: Colors.black12,
+              width: 2,
+              height: ss.height * 0.1,
+            ),
+            SizedBox(
+              width: ss.width * 0.3,
+              child: Text(
+                name,
+                style: TextDecor.titleTextDecoration,
+                softWrap: true,
+                maxLines: 2,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
