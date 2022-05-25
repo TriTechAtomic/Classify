7b9d35ea0080ad828eb173c00663b54ad01bfb8f
Akash <akashvsh13@gmail.com>
2022-05-25 11:53:52 +0530

    abc

--- lib/screens/teacher/signup.dart
+++ lib/screens/teacher/signup.dart
@@ -1,3 +1,5 @@
+import 'package:classify/assets/asset_register.dart';
+import 'package:classify/screens/widgets/form_heading.dart';
 import 'package:flutter/material.dart';
 
 class TeacherSignup extends StatelessWidget {
@@ -5,6 +7,12 @@ class TeacherSignup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+            body: Container(
+      child: Column(
+        children: const [FormHeading(name: "For\nTeachers")],
+      ),
+    )));
   }
 }
