5b527e62940f0f736d4e111ee91a8e5e0fd7ff3b
Akash <akashvsh13@gmail.com>
2022-05-30 19:01:17 +0530

    Manage Courses added

--- lib/screens/admin/features/manage_courses.dart
+++ lib/screens/admin/features/manage_courses.dart
@@ -1,12 +1,48 @@
+import 'package:classify/utils/buttons.dart';
+import 'package:classify/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class ManageCourses extends StatelessWidget {
   const ManageCourses({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Courses manages from here"),
-    );
+    return SafeArea(
+        child: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+                appBar: AppBar(
+                  title: const Text("Manage Courses",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  bottom: const TabBar(
+                    isScrollable: true,
+                    tabs: [
+                      Tab(text: "Create New"),
+                      Tab(text: "Edit Existing"),
+                      Tab(text: "Delete"),
+                      Tab(text: "Test Held"),
+                    ],
+                  ),
+                ),
+                body: const TabBarView(
+                  children: [
+                    Scaffold(
+                      body: CustomButtons(
+                          text: "User Template", height: 250, width: 250),
+                    ),
+                    Scaffold(
+                      body: CustomButtons(
+                          text: "User Template", height: 250, width: 250),
+                    ),
+                    Scaffold(
+                      body: CustomButtons(
+                          text: "User Template", height: 250, width: 250),
+                    ),
+                    Scaffold(
+                      body: CustomButtons(
+                          text: "User Template", height: 250, width: 250),
+                    ),
+                  ],
+                ))));
   }
 }
