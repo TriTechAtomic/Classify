46cdea6d6a5c6723d358a71fef6c26802baa9c05
xxparthparekhxx <xx.parthparekh.xx@gmail.com>
2022-06-03 10:04:01 +0000

    removed flutter auto start for now

--- sqlstarter/sqlstarter.sh
+++ sqlstarter/sqlstarter.sh
@@ -1,8 +1,6 @@
 sudo apt update ;
 sudo apt install -y postgresql postgresql-client;
 sudo pg_ctlcluster 12 main start;
-cat ./passwd.txt | sudo passwd postgres;
-sudo su -l postgres;
 psql -c "CREATE USER parth WITH SUPERUSER PASSWORD 'xxparthparekhxx_420';";
 psql -c "create database classify;";
 
