50a58821632dc195e7b02e102746da13ca172d62
xxparthparekhxx <xx.parthparekh.xx@gmail.com>
2022-06-03 10:15:44 +0000

    up

--- sqlstarter/sqlstarter.sh
+++ sqlstarter/sqlstarter.sh
@@ -1,7 +1,8 @@
 sudo apt update ;
 sudo apt install -y postgresql postgresql-client;
 sudo pg_ctlcluster 12 main start;
-psql -c "CREATE USER parth WITH SUPERUSER PASSWORD 'xxparthparekhxx_420';";
+sudo su postgres ;
+psql -c "CREATE USER parth WITH SUPERUSER PASSWORD 'xxparthparekhxx_420';"; 
 psql -c "create database classify;";
 
 
