9d5c81ffbbc055c12dc41f687b4310dd579bbd28
Parth Parekh <xx.parthparekh.xx@gmail.com>
2022-06-19 17:05:00 +0530

    newapiurl

--- lib/screens/common/widgets.dart
+++ lib/screens/common/widgets.dart
@@ -1,19 +1,17 @@
 import 'package:classify/provider/authprovider.dart';
-import 'package:classify/screens/admin/admin_side_drawer.dart' as d;
 import 'package:classify/screens/common/signin.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 logoutButton(context) {
-  return d.MenuItem(
-    title: "Logout",
-    icon: const Icon(Icons.logout),
-    press: () async {
+  return ListTile(
+    title: const Text("Logout"),
+    leading: const Icon(Icons.logout),
+    onTap: () async {
       await (await SharedPreferences.getInstance()).clear();
       Provider.of<Auth>(context, listen: false).logout();
       Navigator.of(context).pushNamed(Signin.routename);
     },
-    mode: null,
   );
 }
